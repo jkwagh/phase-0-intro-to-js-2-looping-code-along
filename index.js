398d4844452c772c083885809942889428d1d38b
Jeremiah <kwagh.jeremiah@gmail.com>
2023-09-22 10:39:24 -0400

    complete lab assigment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function writeCards(senderNames, eventName) {
+    const messages = [];
+    for (let i = 0; i < senderNames.length; i++) {
+        messages.push(`Thank you, ${senderNames[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return messages;
+}
+
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+function countDown(startNumber) {
+    while (startNumber >= 0) {
+        console.log(startNumber--);
+    }
+}
